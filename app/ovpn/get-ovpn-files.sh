a11ea6512323c0f085fdf64ef01cdd36d1a4ce2f
Mads <>
2021-09-24 09:03:44 +0200

    Update get-ovpn-files.sh
    This will hopefully fix some of the issues with trying to redownloading the OVPN files over and over again. Now it should only do it when:
    1. the file don't already exsist.
    2. if the current file is older than two hours.

--- app/ovpn/get-ovpn-files.sh
+++ app/ovpn/get-ovpn-files.sh
@@ -2,26 +2,42 @@
 
 . /app/date.sh --source-only
 
-echo "$(adddate) INFO: Download and extract ovpn files"
-
-#Create directory if no volume is done
-mkdir -p /app/ovpn/config
-
-#First remove files if exists
-rm -rf ${OVPN_CONFIG_DIR}/ovpn*
-
-#Curl download ovpn files from NordVPN
-curl -s -o ${OVPN_CONFIG_DIR}/ovpn.zip ${OVPN_FILES}
-
-#Unzip files
-unzip -q ${OVPN_CONFIG_DIR}/ovpn.zip -d ${OVPN_CONFIG_DIR}
-
-#Print out logging
-if [ $? -eq 0 ]; then
-    echo "$(adddate) INFO: OVPN files successfully unzipped to $OVPN_CONFIG_DIR"
+# check if the file exists
+if [ -f ${OVPN_CONFIG_DIR}/ovpn.zip ]; then
+  #the file exists continue checking if its older than two hours.
+  if test `find ${OVPN_CONFIG_DIR}/ovpn.zip -mmin +120` then
+    download_files()
+  else
+    echo "$(adddate) INFO: Skipping downloading OVPN files - as they are not older than two hours."
+  fi
 else
-    echo "$(adddate) ERROR: OVPN files unzipped failed!"
+  #the files don't exists contiue to download and extract them.
+  download_files()
 fi
 
-#Remove current zip
-rm -rf ${OVPN_CONFIG_DIR}/ovpn.zip
+function download_files() {
+  # the current ovpn zip file is more than two hours old.
+  echo "$(adddate) INFO: Downloading new OVPN files."
+
+  #Remove current zip
+  rm -rf ${OVPN_CONFIG_DIR}/ovpn.zip
+
+  #Create directory if no volume is done
+  mkdir -p /app/ovpn/config
+
+  #First remove files if exists
+  rm -rf ${OVPN_CONFIG_DIR}/ovpn*
+
+  #Curl download ovpn files from NordVPN
+  curl -s -o ${OVPN_CONFIG_DIR}/ovpn.zip ${OVPN_FILES}
+
+  #Unzip files
+  unzip -q ${OVPN_CONFIG_DIR}/ovpn.zip -d ${OVPN_CONFIG_DIR}
+
+  #Print out logging
+  if [ $? -eq 0 ]; then
+      echo "$(adddate) INFO: OVPN files successfully unzipped to $OVPN_CONFIG_DIR"
+  else
+      echo "$(adddate) ERROR: OVPN files unzipped failed!"
+  fi
+}
